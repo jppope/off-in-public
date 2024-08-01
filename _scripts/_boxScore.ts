4051807287135f24478f3b69220050abbd64d212
Pope <jp@stormclouddevelopment.com>
2024-08-01 11:07:39 -0700

    late arrivals + updates

--- _scripts/_boxScore.ts
+++ _scripts/_boxScore.ts
@@ -67,8 +67,8 @@ async function processCsv(
   return csvString;
 }
 
-const inputFilePath = "./files/2024-07-25.csv";
-const outputFilePath = "./boxScores/2024-07-25.csv";
+const inputFilePath = "./files/2024-07-31.csv";
+const outputFilePath = "./boxScores/2024-07-31.csv";
 
 processCsv(inputFilePath, outputFilePath)
   .then((csv) =>  Deno.writeTextFile(outputFilePath, csv))
