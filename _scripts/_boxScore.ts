77768e8c17fce167cc3787e538d2621bdea1a487
Pope <jp@stormclouddevelopment.com>
2025-05-23 16:45:11 -0700

    fighting jackrabbits

--- _scripts/_boxScore.ts
+++ _scripts/_boxScore.ts
@@ -67,8 +67,8 @@ async function processCsv(
   return csvString;
 }
 
-const inputFilePath = "./files/2025-05-07.csv";
-const outputFilePath = "./boxScores/2025/2025-05-07.csv";
+const inputFilePath = "./files/2025-05-21.csv";
+const outputFilePath = "./boxScores/2025/2025-05-21.csv";
 
 processCsv(inputFilePath, outputFilePath)
   .then((csv) =>  Deno.writeTextFile(outputFilePath, csv))
